632081d51522780cec0830e3f003145546e1cdb4
Kushal98765 <kushalkumawat001@gmail.com>
2025-06-25 11:28:43 +0530

    last commit

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -1,10 +1,10 @@
-import express from 'express'
-import authMiddleware from '../middleware/authMiddleware.js'
-import { addEmployee, upload, getEmployees, getEmployee, updateEmployee, fetchEmployeesByDepId, deleteEmployee } from '../controllers/employeeController.js'
+import express from 'express';
+import { addEmployee, deleteEmployee, fetchEmployeesByDepId, getEmployee, getEmployees, updateEmployee, upload } from '../controllers/employeeController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 import roleMiddleware from '../middleware/roleMiddleware.js';
 
 
-const router = express.Router()
+const router = express.Router();
 
 router.post('/add', authMiddleware, roleMiddleware('admin'), upload.single('image'), addEmployee);
 router.get('/', authMiddleware, roleMiddleware('admin'), getEmployees);
@@ -13,7 +13,6 @@ router.put('/:id', authMiddleware, roleMiddleware('admin'), updateEmployee);
 router.get('/department/:id', authMiddleware, roleMiddleware('admin'), fetchEmployeesByDepId);
 // Route: DELETE /api/employee/:id
 router.delete('/:id', authMiddleware, roleMiddleware('admin'), deleteEmployee);
-
 
 
 export default router;
